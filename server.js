52b7702b5649189e4de29a8950e443e9b5e7e08c
Angel Rivera <ariv3ra@gmail.com>
2015-04-28 20:49:42 -0400

    Renamed and deleted some files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app =express();
 
 require('./router/app')(app);
-require('./controllers/cylon')(app);
+require('./controllers/servo')(app);
 app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
